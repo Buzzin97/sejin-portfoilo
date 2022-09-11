a16e1b4ca3b97defe79833ab08b3d11e78a51bff
Sejin PPark <x2345x@naver.com>
2022-09-11 22:10:22 +0900

    JS navbar 메뉴 이동하기

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 "use strict";
+// navbar 스크롤링에 따라 색 변하기
 const navbar = document.querySelector('#navbar');
 const home = document.querySelector('#home');
 const homeSize = home.getBoundingClientRect().height
 const navbarHight = navbar.getBoundingClientRect().height
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navsize ${navbarHight}`);
-  if(window.scrollY > homeSize) {
+  if(window.scrollY > navbarHight) {
     navbar.classList.add('navbar--dark')
   } else {
     navbar.classList.remove('navbar--dark')
   }
+})
+
+// navbar 메뉴 클릭시 화면 스크롤링
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
 })
